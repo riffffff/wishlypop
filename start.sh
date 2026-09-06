8ac6acfbf5e5255d1cf30cb787818394e25c1f37
rifaifirdaus <rifaimuhammad2004@gmail.com>
2026-09-06 18:21:53 +0700

    wishlypop v1.1: modify start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo "DATABASE_URL: $DATABASE_URL"
 
 echo ""
 echo "🔧 Running Prisma schema sync (db push)..."
-npx prisma db push --skip-generate
+npx prisma db push 
 
 echo ""
 echo "🚀 Starting Next.js server on 0.0.0.0:${PORT:-3000}..."
